4f715c014e6e6c6f53ae34f980d6f93fc15d8604
tinasot9874 <tinasot9874@gmail.com>
2019-10-08 21:03:50 +0700

    nav scroll jquery

---
+++ resources/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(
+    function() {
+        $('.about-section').waypoint(
+            function(direction) {
+                if (direction == "down") {
+                    $('nav').addClass('sticky');
+                } else {
+                    $('nav').removeClass('sticky');
+                }
+            }, {
+                offset: '600px'
+            }
+        )
+    }
+)
